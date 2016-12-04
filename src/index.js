1009e5869f260c75bc73be1775a4db69f38fa665
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-12-04 13:31:02 +0200

    progress

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import 'newrelic';
 
 import {
   fetchEvents,
@@ -11,11 +12,14 @@ const app = express();
 app.use(morgan('combined'));
 app.use(helmet());
 
-
 app.get('/', (req, res) => {
   fetchEvents()
     .then(events => res.json(events))
     .catch(err => res.status(500).send('error', JSON.stringify(err)));
+});
+
+app.get('/test', (req, res) => {
+  res.send('test');
 });
 
 export default app;
